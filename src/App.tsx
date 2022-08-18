9e77f1a1e93cc61071c0b49fc851161352a00232
Sumi Seo <seo.sumi@favoritemedium.com>
2022-08-18 15:28:06 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Web3 from "web3";
+import { CONTACT_ABI, CONTACT_ADDRESS } from "./config";
+import { AbiItem } from "web3-utils";
 
 function App() {
+  const [account, setAccount] = useState<any>();
+  const [contactList, setContactList] = useState<any>();
+  const [contacts, setContacts] = useState<any[]>([]);
+
+  useEffect(() => {
+    async function load() {
+      const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
+      const accounts = await web3.eth.requestAccounts();
+      setAccount(accounts[0]);
+
+      const contactList = new web3.eth.Contract(
+        CONTACT_ABI as AbiItem[],
+        CONTACT_ADDRESS
+      );
+
+      setContactList(contactList);
+      console.log(contactList);
+
+      const counter = await contactList.methods.count().call();
+
+      for (var i = 1; i <= counter; i++) {
+        const contact = await contactList.methods.contacts(i).call();
+        setContacts((contacts) => [...contacts, contact]);
+      }
+    }
+
+    load();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Your account is: {account}
+      <h1>Contacts</h1>
+      <ul></ul>
     </div>
   );
 }
